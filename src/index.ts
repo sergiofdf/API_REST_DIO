659cbcfa6388eafad6e9c3bda4890cd9a6e5b823
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2021-10-31 17:10:09 -0300

    Adicionando o typescript e incrementando forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Executei');
+
+import express, { Request, Response, NextFunction } from 'express';
+ 
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction)=>{
+    res.status(200).send({ foo: 'bar! Sucesso total!' });
+});
+
+app.listen(3000, ()=>{
+  console.log('Aplicação execuntado na porta 3000!');
+});
